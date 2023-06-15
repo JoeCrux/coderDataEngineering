36770de1c7720eebce3c3ab21f7c4eccefedd735
jlc77koan <jose.l.cruz77@gmail.com>
2023-06-15 13:46:52 -0300

    se agrega script sql y print de pantalla, sobre creacion de Tabla

---
+++ scriptCreacionTablaDesafioClase5.sql
@@ -0,0 +1,10 @@
+create table company_earnings(
+		actual decimal NULL,
+		estimate decimal NULL,
+		periodo date NULL,
+		cuarto int NULL,
+		surprise decimal NULL,
+		surprisePercent decimal NULL,
+		symbol varchar(15) NULL,   
+		anio int NULL
+	)
