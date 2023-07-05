2ecbc12bbe1d2b6279bbbfff69de3932d094e450
JoeCrux <118003007+JoeCrux@users.noreply.github.com>
2023-07-05 20:56:16 -0300

    actualización script inserción datos en Redshift

--- scriptDesafioClase7.py
+++ scriptDesafioClase7.py
@@ -11,13 +11,15 @@ def execute_query(connection, query):
         connection.close()
     except Exception as err:
         print(f"Unexpected {err=}, {type(err)=}")
-    #except Error as e:
-    #    print(f"Error '{e}' ha ocurrido")
 
 # conexion API
 finnhub_client = finnhub.Client(api_key="bqoji3nrh5rced4gaukg")
+
 # datos API
 df =  pd.DataFrame(finnhub_client.company_earnings('TSLA', limit=5)) 
+
+#ordeno dataframe 
+dforder = df.sort_values(['period', 'quarter'])
 
 #constantes datos acceso a Redshift
 hostname= 'data-engineer-cluster.cyhh5bfevlmn.us-east-1.redshift.amazonaws.com'
@@ -28,13 +30,18 @@ port_id= '5439'
 
 # Conexion a redshift
 conn = psycopg2.connect(host=hostname, dbname=database, user=username, password=pwd, port=port_id)
-args=""
-for i in range(len(df)):
-    if i == (len(df) - 1):
-        args =args + str('(')+str(df.iloc[i]['actual'])+','+str(df.iloc[i]['estimate'])+','+"'"+str(df.iloc[i]['period'])+"'"+','+str(df.iloc[i]['quarter'])+','+str(df.iloc[i]['surprise'])+','+str(df.iloc[i]['surprisePercent'])+','+"'"+str(df.iloc[i]['symbol'])+"'"+','+str(df.iloc[i]['year'])+');'
-    else:
-        args =args + str('(')+str(df.iloc[i]['actual'])+','+str(df.iloc[i]['estimate'])+','+"'"+str(df.iloc[i]['period'])+"'"+','+str(df.iloc[i]['quarter'])+','+str(df.iloc[i]['surprise'])+','+str(df.iloc[i]['surprisePercent'])+','+"'"+str(df.iloc[i]['symbol'])+"'"+','+str(df.iloc[i]['year'])+'),'
 
+# Armado de string de valores a insertar
+args=""
+for i in range(len(dforder)):
+    if i == (len(dforder) - 1):
+        args =args + str('(')+str(dforder.iloc[i]['actual'])+','+str(dforder.iloc[i]['estimate'])+','+"'"+str(dforder.iloc[i]['period'])+"'"+','+str(dforder.iloc[i]['quarter'])+','+str(dforder.iloc[i]['surprise'])+','+str(dforder.iloc[i]['surprisePercent'])+','+"'"+str(dforder.iloc[i]['symbol'])+"'"+','+str(dforder.iloc[i]['year'])+');'
+    else:
+        args =args + str('(')+str(dforder.iloc[i]['actual'])+','+str(dforder.iloc[i]['estimate'])+','+"'"+str(dforder.iloc[i]['period'])+"'"+','+str(dforder.iloc[i]['quarter'])+','+str(dforder.iloc[i]['surprise'])+','+str(dforder.iloc[i]['surprisePercent'])+','+"'"+str(dforder.iloc[i]['symbol'])+"'"+','+str(dforder.iloc[i]['year'])+'),'
+
+#formateo comando sql
 query5="""INSERT INTO jolcrux77_coderhouse.company_earnings (actual,estimate,periodo,cuarto,surprise,surprisepercent,symbol,anio) VALUES """+ args
 
+#llamo a funcion para ejecutar comando sql
 execute_query(conn, query5)
+
